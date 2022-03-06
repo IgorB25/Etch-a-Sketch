2959916b2569dd92f900f67bb1ee07bd5410c304
Igor <igorbulka5@gmail.com>
2022-03-06 10:56:57 +0100

    fixed grid layout

--- script.js
+++ script.js
@@ -1,28 +1,23 @@
 const reset = document.querySelector('.reset');
 const board = document.querySelector('#board');
 const menu = document.querySelector('.menu');
-let SizeValue;
+let SizeValue = 2;
 
-SizeButton.addEventListener('click',() =>{
-    
-    SizeValue = SizeInput.value;
-    ResetBoard();
-    CreateBoard(SizeValue);
-})
+
 
     
 
 
 
-function CreateBoard(SizeValue){
+function CreateBoard(){
     
-
+    console.log(SizeValue);
     for(let i = 0;i <SizeValue*SizeValue; i++){
-
+        
         let cell = document.createElement('div');
         board.appendChild(cell).classList = ('cell');
-        board.style.gridTemplateColumns = "repeat(SizeValue, 1fr)";
-        board.style.gridTemplateRows = "repeat(SizeValue, 1fr)";
+        board.style.gridTemplateColumns = `repeat(${SizeValue}, 1fr)`;
+        board.style.gridTemplateRows = `repeat(${SizeValue}, 1fr)`;
         cell.addEventListener('mouseover',() =>{
             cell.style.cssText = 'background-color: black'
         })
@@ -30,7 +25,7 @@ function CreateBoard(SizeValue){
 };
 
 function ResetBoard(){
-   
+  
     let reset = document.querySelectorAll(".cell");
     reset.forEach(cell => cell.remove())
     CreateBoard();
@@ -40,4 +35,14 @@ reset.addEventListener('click',ResetBoard)
 
 
 
-CreateBoard(SizeValue);
+SizeButton.addEventListener('click',() =>{
+    SizeValue = SizeInput.value;
+    if(SizeValue > 100 || SizeValue < 2){
+        alert("Size must be between 2 and 100")
+    }
+        else
+        ResetBoard();
+        
+})
+
+CreateBoard();
